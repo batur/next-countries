5ccfb9539bef99d364929ce64b8658070ebb537f
Batur <halidbaturakkurt@gmail.com>
2022-07-17 22:34:06 +0300

    feat: install environment
    - Jotai with React-Query
    - MUI
    - Next-Pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = {
+	pwa: {
+		dest: "public",
+		runtimeCaching,
+	},
+	reactStrictMode: true,
+	swcMinify: true,
+};
+
+module.exports = nextConfig;
