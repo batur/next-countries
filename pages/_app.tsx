5cef8314e5db582feee32c5db91cd2584dfa13da
Batur <halidbaturakkurt@gmail.com>
2022-07-17 23:28:58 +0300

    feat: implement MUI basics & Jotai
    - Theme Provider with theme detector
    - Change prettier config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { Provider as JotaiProvider } from 'jotai';
+import {
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  useMediaQuery,
+} from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isDarkModeEnabled = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = createTheme({
+    palette: {
+      mode: isDarkModeEnabled ? 'dark' : 'light',
+    },
+  });
+
+  return (
+    <JotaiProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </JotaiProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
