fdf8a5264c0996e9c9f5fc13b0e1c5f4d9b3e7b5
Batur <halidbaturakkurt@gmail.com>
2022-07-17 22:56:04 +0300

    feat: implement tools
    - Eslint + Prettier with Google Style
    - Global Type Declaration
    - TS Config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 const nextConfig = {
-	pwa: {
-		dest: "public",
-		runtimeCaching,
-	},
-	reactStrictMode: true,
-	swcMinify: true,
-};
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
