d63c1479155fb92038b338db798374545630df05
Batur <halidbaturakkurt@gmail.com>
2022-07-19 03:26:56 +0300

    feat: implement hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,5 +1,11 @@
-import { Box } from '@mui/material';
-import { useTheme, Container } from '@mui/system';
+import {
+  useTheme,
+  Box,
+  Container,
+  Typography,
+  Link as MuiLink,
+  Button,
+} from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Image from 'next/image';
 import React from 'react';
@@ -9,14 +15,40 @@ const Hero = () => {
   const breakpoint = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
-    <Box>
-      <Container maxWidth="lg"></Container>
+    <>
+      <Container
+        component="section"
+        maxWidth="lg"
+        sx={{
+          position: 'fixed',
+          zIndex: 10,
+          width: '100%',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <Button
+          variant="contained"
+          color="info"
+          fullWidth
+          size="large"
+          sx={{
+            width: '40%',
+          }}
+        >
+          Browse
+        </Button>
+      </Container>
       <Box
         width={'100vw'}
         height={breakpoint ? '100%' : '100vh'}
         position="fixed"
         left={0}
         top={0}
+        zIndex={-10}
       >
         <Image
           src="/images/hero-large.jpeg"
@@ -27,8 +59,36 @@ const Hero = () => {
           objectFit="cover"
           priority={true}
         />
+        <Box
+          position={'absolute'}
+          bottom={16}
+          right={16}
+          color="GrayText"
+          boxShadow={3}
+          padding={2}
+          sx={{
+            opacity: 0.5,
+          }}
+        >
+          <Typography>
+            Photo by{' '}
+            <MuiLink
+              color="inherit"
+              href="https://unsplash.com/@aslylmz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            >
+              Aslı Yılmaz{' '}
+            </MuiLink>
+            on{' '}
+            <MuiLink
+              color="inherit"
+              href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            >
+              Unsplash
+            </MuiLink>
+          </Typography>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
